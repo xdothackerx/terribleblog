08a5d46487534e2d5eec55630494b6312a3d899b
xdothackerx <xdothackerx@gmail.com>
2014-06-17 19:29:49 -0700

    Fixed issue with post controller.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,7 +13,7 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
-    @post = Post.find(params[:id]).includes(:comments)
+    @post = Post.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
